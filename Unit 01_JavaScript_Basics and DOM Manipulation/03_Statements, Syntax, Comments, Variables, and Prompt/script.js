671421cd8137dd892fea02763bf39f2558736e78
BrandonHernandez2 <hernabra000@warren.k12.in.us>
2024-01-10 09:01:17 -0500

    initial commit

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/03_Statements, Syntax, Comments, Variables, and Prompt/script.js
@@ -0,0 +1,7 @@
+let firstName = prompt("Enter Your First Name:");
+//Do the same for last name
+let lastName = prompt("Enter Your Last Name:")
+
+let fullName = firstName + " " + lastName
+console.log(fullName)
+document.getElementById("name").innerHTML = fullName
