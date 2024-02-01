0823ad29fe2bd442ab18f8ffd081c05e867bb630
BrandonHernandez2 <hernabra000@warren.k12.in.us>
2024-02-01 09:08:47 -0500

    update

---
+++ Unit 04_Objects, Events, Strings, and Numbers/02_Events Basics/script.js
@@ -0,0 +1,8 @@
+function onClick(){
+    document.getElementById("pic").hidden = true;
+}
+
+function myFunction() {
+    var x = document.getElementById("mySelect").value;
+    document.getElementById("demo").innerHTML = "You selected: " + x;
+  }
