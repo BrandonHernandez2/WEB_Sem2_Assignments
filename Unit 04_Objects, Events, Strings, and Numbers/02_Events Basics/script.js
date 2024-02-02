59f3672bee932c088aeecc7df7797c46b88a76c1
BrandonHernandez2 <hernabra000@warren.k12.in.us>
2024-02-02 08:55:07 -0500

    initial commit

--- Unit 04_Objects, Events, Strings, and Numbers/02_Events Basics/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/02_Events Basics/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function onClick(){
     document.getElementById("pic").hidden = true;
 }
@@ -7,23 +6,3 @@ function myFunction() {
     var x = document.getElementById("mySelect").value;
     document.getElementById("demo").innerHTML = "You selected: " + x;
   }
-=======
-function mouseOverHandler() {
-    document.getElementById("demo").innerHTML = "Hello Again"
-}
-
-function mouseOutHandler() {
-    document.getElementById("kitties").hidden = false;
-}
-
-function keyDownHandler() {
-    let x = 5
-    let y = x + 7
-    document.getElementById("demo").innerHTML = y
-    
-}
-
-function keyUpHandler() {
-    document.getElementById("kitties").hidden = true;
-}
->>>>>>> d99e6f96bc0e55fd5fe5368b510bd8d9fe5bedde
