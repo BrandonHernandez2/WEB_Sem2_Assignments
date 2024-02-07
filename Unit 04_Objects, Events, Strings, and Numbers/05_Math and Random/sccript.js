8341384be5fab20a0539190ed7ea4342a18f1fe8
BrandonHernandez2 <hernabra000@warren.k12.in.us>
2024-02-07 08:55:54 -0500

    initial commit

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/sccript.js
+++
@@ -1,7 +0,0 @@
-function takeSquareRoot(){
-    let number1 = document.getElementById("input1").value;
-    
-
-    let squareRoot = Math.sqrt(number)
-    document.getElementById("result").innerHTML = squareRoot;
-}
--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -7,4 +7,23 @@ function takeSquareRoot() {
     
 }
 
-// Make another function for takeAbsoluteValue
+function roundDown(){
+    let number = +document.getElementById("input1").value;
+
+    let round = Math.floor(number);
+    document.getElementById("result").innerHTML = round
+}
+
+function absoValue(){
+    let number = +document.getElementById("input1").value;
+
+    let absolute = Math.abs(number);
+    document.getElementById("result").innerHTML = absolute;
+}
+
+function sin(){
+    let number = +document.getElementById("input1").value;
+    
+    let sine = Math.sin(number);
+    document.getElementById("result").innerHTML = sine;
+}
